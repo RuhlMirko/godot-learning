d84c2c20ac0bc3ba929f8332c653711e497c8c36
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-05 00:32:18 -0300

    Added animation for idle jump and run

--- tutorial/scripts/player.gd
+++ tutorial/scripts/player.gd
@@ -5,6 +5,8 @@ const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 var doubleJump = true
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if is_on_floor():
@@ -14,15 +16,29 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed('ui_accept') and not is_on_floor() and doubleJump:
+		
+	if Input.is_action_just_pressed("jump") and not is_on_floor() and doubleJump:
 		doubleJump = false
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jumping")
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
