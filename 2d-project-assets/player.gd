cef3baef18c8d83fc113190cf6873a773b208329
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-06 13:53:32 -0300

    Added animations and background

--- 2d-project-assets/player.gd
+++ 2d-project-assets/player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+const BASE_SPEED = 600
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * BASE_SPEED
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
