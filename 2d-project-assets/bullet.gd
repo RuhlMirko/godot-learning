426cdb94b328a03334d148484c64c13687c5ea24
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-07 12:44:21 -0300

    Added bullet logic

---
+++ 2d-project-assets/bullet.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+const BULLET_SPEED = 1000
+var travelled_distance = 0
+const MAX_RANGE = 1200
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * BULLET_SPEED * delta
+	
+	travelled_distance += BULLET_SPEED * delta
+	if travelled_distance > MAX_RANGE:
+		queue_free()
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
