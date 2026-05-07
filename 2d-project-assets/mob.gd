1192755942dd66686b066f9f4caf9cc3ea11f57a
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-07 13:20:45 -0300

    Gun automatically shoots

--- 2d-project-assets/mob.gd
+++ 2d-project-assets/mob.gd
@@ -1,10 +1,26 @@
 extends CharacterBody2D
 
 const MOB_SPEED = 300.0
+var base_health = randf_range(20, 100)
 
 @onready var player = $"../../Player"
+
+func _ready():
+	%Slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * MOB_SPEED
 	move_and_slide()
+	
+func take_damage()->void:
+	%Slime.play_hurt()
+	base_health -= 20	
+	if base_health <= 0:
+		queue_free()
+		
+		const SMOKE_EXPLOSION = preload("uid://dhmhmrth6rdce")
+		var smoke = SMOKE_EXPLOSION.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
+	
--- 2d-project-assets/player.gd
+++ 2d-project-assets/player.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
-const BASE_SPEED = 600
+signal health_depleted
+
+const BASE_SPEED : int= 600
+const BASE_HEALTH : float = 100.0
+var curr_health: float = BASE_HEALTH
+const DAMEGE_RATE: float = 5.0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
@@ -11,3 +16,12 @@ func _physics_process(delta: float) -> void:
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
+	
+	
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size()>0:
+		curr_health -= DAMEGE_RATE * overlapping_mobs.size() * delta
+		if curr_health <= 0.0:
+			health_depleted.emit()
+			
+	
