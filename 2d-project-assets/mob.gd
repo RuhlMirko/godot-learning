ef62951c6c847487531b95e5e825839c7c92e0be
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-07 12:08:31 -0300

    Added mob movement and fixed background

--- 2d-project-assets/mob.gd
+++ 2d-project-assets/mob.gd
@@ -1,1 +1,10 @@
 extends CharacterBody2D
+
+const MOB_SPEED = 300.0
+
+@onready var player = $"../../Player"
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * MOB_SPEED
+	move_and_slide()
