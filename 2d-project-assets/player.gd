acb087423784cb47eabe8b271ba0f050ce7e0b4d
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-07 14:58:16 -0300

    Added health bar

--- 2d-project-assets/player.gd
+++ 2d-project-assets/player.gd
@@ -5,7 +5,8 @@ signal health_depleted
 const BASE_SPEED : int= 600
 const BASE_HEALTH : float = 100.0
 var curr_health: float = BASE_HEALTH
-const DAMEGE_RATE: float = 5.0
+const DAMAGE_RATE: float = 25.0
+
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
@@ -15,12 +16,13 @@ func _physics_process(delta: float) -> void:
 	if velocity.length() > 0.0:
 		%HappyBoo.play_walk_animation()
 	else:
-		%HappyBoo.play_idle_animation()
-	
+		%HappyBoo.play_idle_animation()	
 	
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size()>0:
-		curr_health -= DAMEGE_RATE * overlapping_mobs.size() * delta
+		curr_health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = curr_health
+		
 		if curr_health <= 0.0:
 			health_depleted.emit()
 			
