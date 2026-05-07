3b0d527fbc3f9d2b3039ceb64d69520605fbd7a2
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-07 19:33:18 -0300

    Added more sprites

--- 2d-project-assets/bullet.gd
+++ 2d-project-assets/bullet.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-const BULLET_SPEED = 1000
+const BULLET_SPEED = 2000
 var travelled_distance = 0
 const MAX_RANGE = 1200
 
@@ -11,7 +11,6 @@ func _physics_process(delta: float) -> void:
 	travelled_distance += BULLET_SPEED * delta
 	if travelled_distance > MAX_RANGE:
 		queue_free()
-
 
 
 func _on_body_entered(body: Node2D) -> void:
