16472d6b8ebd1b8c1504139212eaec425afca1da
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-07-26 13:54:28 -0300

    Initial commit of top down shooter

--- 2d-Beat'em/beat-em-up/scenes/characters/basic_enemy.gd
+++ 2d-Beat'em/beat-em-up/scenes/characters/basic_enemy.gd
@@ -3,7 +3,14 @@ extends Character
 
 @export var player : Player
 
+var player_slot : EnemySlot = null
+
 func handle_input() -> void:
 	if player != null and can_move():
-		var direction := (player.position - position).normalized()
-		velocity = direction * speed
+		
+		if player_slot == null :
+			player_slot = player.reserve_slot(self)
+		
+		if player_slot != null :
+			var direction := (player_slot.position - global_position).normalized()
+			velocity = direction * speed
