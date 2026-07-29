b1d7474e4c4f64d202eaf22d11d075d3479dfa1c
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-07-29 04:28:11 -0300

    Added timer to laser

--- shooter/shooter/scenes/projectiles/laser.gd
+++ shooter/shooter/scenes/projectiles/laser.gd
@@ -3,5 +3,16 @@ extends Area2D
 @export var speed : int = 2000
 var direction : Vector2
 
+func _ready() -> void:
+	$Timer.start()
+
 func _process(delta: float) -> void:
 	position += direction * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if "hit" in body:
+		body.hit()
+	queue_free()
+
+func _on_timer_timeout() -> void:
+	queue_free()
