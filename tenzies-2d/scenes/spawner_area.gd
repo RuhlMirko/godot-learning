c0a44e19a1adf5880f4c7d6bf317c806560ee47f
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-07-30 13:48:05 -0300

    Added wip dice sprite setter

--- tenzies-2d/scenes/spawner_area.gd
+++ tenzies-2d/scenes/spawner_area.gd
@@ -2,11 +2,27 @@ extends Area2D
 
 @onready var timer := $Timer
 
-#timer.start()
-
-func  _on_ready()->void:
+func _on_ready()->void:
 	timer.start()
+	
 
 func _on_timer_timeout() -> void:
-	#set_newDiceSet()
-	print("Timer called")
+	var dices = $Dices.get_children()
+	print(dices, dices[0].frame)
+	#print("Timer called")
+	print(newDiceSet())
+
+func newDiceSet()->Array[int]:
+	# Generate array with random range values
+	var diceAmount : int = 3
+	var newDices: Array[int] = []
+	newDices.resize(diceAmount)
+	for i in range(diceAmount):
+		newDices[i] = randi() % 6
+		
+	#var newDices = $Dices.get_children()
+	#for i in range(diceAmount):
+		#newDices[i].frame = randi() % 6
+	
+	return newDices
+	
