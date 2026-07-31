af3ac8f896184d6fdefb506fe60e55c89fa7dc12
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-07-31 12:18:23 -0300

    Added signal practice

--- udemy/scenes/hobbit.gd
+++ udemy/scenes/hobbit.gd
@@ -1,6 +1,10 @@
-class_name  Hobbit
+class_name Hobbit
 extends Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate(10 * delta)
+
+
+func _on_timer_timeout() -> void:
+	hide()
--- udemy/scenes/wizard.gd
+++ udemy/scenes/wizard.gd
@@ -1,11 +1,14 @@
 extends Node2D
 class_name Wizard
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_timer_timeout() -> void:
+	show()
