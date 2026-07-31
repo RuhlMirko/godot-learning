c0a21dc1cc317cf0b4dc673a91a1d9df48d47419
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-07-31 12:41:46 -0300

    Added input challenge

--- udemy/scenes/hobbit.gd
+++ udemy/scenes/hobbit.gd
@@ -1,6 +1,11 @@
 class_name Hobbit
 extends Node2D
 
+signal hobbit_spell
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("hit_wizard"):
+		hobbit_spell.emit()
 
 func _ready() -> void:
 	pass
@@ -8,3 +13,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate(PI * delta)
+
+
+func hit_by_spell()->void:
+	scale = Vector2(0.5,0.5)
+	rotation = 0
+	set_process(false)
--- udemy/scenes/wizard.gd
+++ udemy/scenes/wizard.gd
@@ -1,6 +1,7 @@
 extends Node2D
 class_name Wizard
 
+signal cast_spell
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +15,12 @@ func _process(delta: float) -> void:
 
 func _on_spell_timer_timeout() -> void:
 	print("Spell was cast")
+	cast_spell.emit()
 
 
 func _on_reveal_timer_timeout() -> void:
 	show()
+
+func hit_by_spell()->void:
+	scale = Vector2(0.5,0.5)
+	set_process(false)
