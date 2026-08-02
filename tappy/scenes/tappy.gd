011cc1d15bdaf9953a1a0bbf240706f3b165070c
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-08-02 00:22:21 -0300

    Added gates with lasers

--- tappy/scenes/tappy.gd
+++ tappy/scenes/tappy.gd
@@ -4,6 +4,8 @@ var _gravity : float = ProjectSettings.get("physics/2d/default_gravity")
 var _jumped: bool = false
 const JUMP_FORCE : float = -350.0
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		
@@ -14,4 +16,5 @@ func _physics_process(delta: float) -> void:
 	if _jumped:
 		velocity.y += JUMP_FORCE
 		_jumped = false
+		animation_player.play("fly")
 	move_and_slide()
