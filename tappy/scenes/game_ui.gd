57a4b0560d2a555c60a374e7f005f1f2a5eca72b
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-08-02 23:59:20 -0300

    Added game over sound

--- tappy/scenes/game_ui.gd
+++ tappy/scenes/game_ui.gd
@@ -1,13 +1,27 @@
 extends Control
 
 @onready var game_over_label: Label = $MarginContainer/GameOverLabel
+@onready var change_label_timer: Timer = $ChangeLabelTimer
+@onready var game_over_label_2: Label = $MarginContainer/GameOverLabel2
+@onready var game_over_sound: AudioStreamPlayer = $gameOverSound
+
+var can_go_main:bool= false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("dev"):
+	if event.is_action_pressed("jump") and can_go_main:
 		GameManager.load_main_screen()
 
 func _ready() -> void:
 	Signalhub.tappy_died.connect(game_over)
+	can_go_main = false
 
 func game_over()->void:
 	game_over_label.show()
+	change_label_timer.start()
+	game_over_sound.play()
+
+
+func _on_change_label_timer_timeout() -> void:
+	can_go_main = true
+	game_over_label.hide()
+	game_over_label_2.show()
--- tappy/scenes/tappy.gd
+++ tappy/scenes/tappy.gd
@@ -9,7 +9,6 @@ const JUMP_FORCE : float = -350.0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
-		
 		_jumped = true
 
 func _physics_process(delta: float) -> void:
