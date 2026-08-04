9cd1d9ef2aa4f22b1e3197ec63685c820baad309
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-08-04 18:42:32 -0300

    Added release mechanic

--- AngryAnimals/scenes/animal.gd
+++ AngryAnimals/scenes/animal.gd
@@ -3,19 +3,21 @@ extends RigidBody2D
 
 const DRAG_LIMIT_MAX:Vector2= Vector2(0,60)
 const DRAG_LIMIT_MIN:Vector2= Vector2(-60,0)
+const IMPLULSE_MULT : float = 15.0
+
 @onready var label: Label = $Label
 @onready var arrow: Sprite2D = $Arrow
 @onready var stretch_sound: AudioStreamPlayer = $StretchSound
+@onready var launch_sound: AudioStreamPlayer = $LaunchSound
+
 var _start : Vector2 = Vector2.ZERO
 var _drag_start : Vector2 = Vector2.ZERO
 var _is_dragging: bool = false
 var _drag_vector: Vector2 = Vector2.ZERO
 
-
-func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed("drag"):
-		#freeze = false
-	pass
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("drag") and _is_dragging:
+		start_release()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,10 +27,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var debug_str = "Fr: %s\nCC: %d\nSl: %s\n" % [freeze, get_contact_count(), sleeping]
 	debug_str += "is_dragging: %s\n drag_start:%.0f,%.0f\n drag_v2:%.0f,%.0f" % [_is_dragging, _drag_start.x, _drag_start.y, _drag_vector.x, _drag_vector.y]
+	#debug_str += "\n drag_v2:%.0f,%.0f" % [_is_dragging, _drag_start.x, _drag_start.y, _drag_vector.x, _drag_vector.y]
 	label.text = debug_str
 
 func _physics_process(delta: float) -> void:
 	if _is_dragging: handle_dragging()
+	
+
+func start_release()->void:
+	arrow.hide()
+	_is_dragging = false
+	freeze = false
+	apply_central_impulse(calculate_impulse())
+	launch_sound.play()
+
+func calculate_impulse()->Vector2:
+	return (_drag_vector * IMPLULSE_MULT ) * -1
 
 func start_dragging()->void:
 	arrow.show()
