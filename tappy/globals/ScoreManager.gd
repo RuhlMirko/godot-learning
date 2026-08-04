f41129dcc5d25348d12caf557bfd311791e31355
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-08-04 01:35:14 -0300

    Added save files

--- tappy/globals/ScoreManager.gd
+++ tappy/globals/ScoreManager.gd
@@ -1,16 +1,22 @@
 extends Node
 
+const SAVE_PATH : String = "user://tappy_game.dat"
+
 var _score : int = 0
-var high_score : int = 0:
-	set(value):
-		if value > high_score:
-			high_score = value
+var high_score : int = 0
+	#set(value):
+		#if value > high_score:
+			#high_score = value
+			#save_to_file()
 
 func _ready() -> void:
+	load_from_file()
 	Signalhub.tappy_died.connect(on_tappy_died)
 
 func on_tappy_died()->void:
-	high_score = _score
+	if _score > high_score:
+			high_score = _score
+			save_to_file()
 	reset_score()
 
 func reset_score()->void:
@@ -20,3 +26,17 @@ func add_point()->void:
 	_score += 1
 	Signalhub.scored_gate(_score)
 	
+func save_to_file():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if !file:
+		push_error("Save file not found")
+		return
+	file.store_32(high_score)
+
+func load_from_file():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if !file:
+		push_error("Save file not found")
+		return
+	high_score = file.get_32()
+	return high_score
