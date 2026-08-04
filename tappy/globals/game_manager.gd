b4016e0ebfaa2e1e65220ed4b78f58141ff84cbf
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-08-04 01:54:44 -0300

    Added loading screen

--- tappy/globals/game_manager.gd
+++ tappy/globals/game_manager.gd
@@ -2,9 +2,27 @@ extends Node
 
 const GAME = preload("uid://cmvi8rt67bmrw")
 const MAIN = preload("uid://cg0767yigyres")
+const LOADING_SCREEN = preload("uid://dy1hpma4jysvn")
+
+#region Original
+#func load_game_screen()->void:
+	#get_tree().change_scene_to_packed(GAME)
+#
+#func load_main_screen()->void:
+	#get_tree().change_scene_to_packed(MAIN)
+#endregion
+
+#region newFeature
+var _next_scene : PackedScene 
+
+func change_to_next()->void:
+	get_tree().change_scene_to_packed(_next_scene)
 
 func load_game_screen()->void:
-	get_tree().change_scene_to_packed(GAME)
+	_next_scene = GAME
+	get_tree().change_scene_to_packed(LOADING_SCREEN)
 
 func load_main_screen()->void:
-	get_tree().change_scene_to_packed(MAIN)
+	_next_scene = MAIN
+	get_tree().change_scene_to_packed(LOADING_SCREEN)
+#endregion
