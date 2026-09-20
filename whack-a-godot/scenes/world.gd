9fc8a62f7fbfa48cba44fc649ce5870d149d7116
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-09-20 01:46:36 -0300

    Added score and timer

--- whack-a-godot/scenes/world.gd
+++ whack-a-godot/scenes/world.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+var score : int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _process(_delta: float) -> void:
+	$CountDownLBL.text = "Time: %d" % $CountdownTimer.time_left
 
+func plus_score():
+	#if $CountdownTimer.time_left != 0:
+	score += 1
+	$ScoreLabel.text = "SCORE: %d" % score
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func minus_score():
+	score -= 1
+
+func _on_countdown_timer_timeout() -> void:
+	$ScoreLabel.text = "FINAL SCORE: %d" % score
+	$Button.visible = false
+
+func _on_button_pressed() -> void:
+	plus_score()
+	#$Button/AnimationPlayer.play("hit")
