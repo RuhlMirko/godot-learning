aab4e5aca9f15aed7f8de2f1f6cf3d8854d768b0
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-09-26 12:56:26 -0300

    Added fumble animation to player

--- quickdraw/scenes/player.gd
+++ quickdraw/scenes/player.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+enum State {Waiting, Shooting, Dying, Fumbling}
+
 signal shot_gun(action)
 @export var action_name = "p1shoot"
-var current_state = "waiting"
+var current_state = State.Waiting
+var can_shoot = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,11 +14,26 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if current_state == "waiting" and Input.is_action_just_pressed(action_name):
-		current_state = "shooting"
-		$AnimationPlayer.play("shoot")
-		shot_gun.emit(action_name)
+	if current_state == State.Waiting and Input.is_action_just_pressed(action_name):
+		if can_shoot:
+			shoot()
+		else:
+			fumble()
+			
+
+func shoot():
+	current_state = State.Shooting
+	$AnimationPlayer.play("shoot")
+	shot_gun.emit(action_name)
+
+func fumble():
+	current_state = State.Fumbling
+	$AnimationPlayer.play("fumble")
 
 func kill():
-	current_state = "dying"
+	current_state = State.Dying
 	$AnimationPlayer.play("die")
+
+func wait():
+	current_state = State.Waiting
+	$AnimationPlayer.play("idle")
